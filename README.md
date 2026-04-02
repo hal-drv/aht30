6da5f25807894ea007aaad3ed5a990ad95b535cb
kkocdko <kkocdko@gmail.com>
2026-04-02 08:22:42 +0800

    AHT10 / AHT40 support

--- README.md
+++ README.md
@@ -1,8 +1,63 @@
 # aht30
 
-Correct AHT10 / AHT20 / AHT30 / AHT40 humidity temperature sensor driver, on embedded-hal, optional async.
+Better AHT10 / AHT20 / AHT30 / AHT40 humidity temperature sensor driver, for rust embedded-hal, optional async.
 
 - Flexable design: configurable I2C address, enable / disable checksum.
-- Optional async: enable the `async` feature, can be used with embassy.
-- Allow read `u32` value for platforms without FPU.
-- Example: ch32v003, esp32c3.
+- Optional async: enable the `async` feature, can be used with [embassy](https://github.com/embassy-rs/embassy).
+- Friendly for no-FPU platform: allow read raw `u32` or `u16` value.
+- Correct and sound: follow official datasheet and routine. Already tested on real hardware.
+
+## Example
+
+### AHT10 async
+
+```rust
+use aht30::{AHT10_DEFAULT_ADDR, Aht10Async};
+use embassy_time::{Delay, Timer};
+use esp_hal::i2c::master::I2c; // or whatever any embedded-hal i2c, like ch32-hal, stm32f4xx-hal, and linux-embedded-hal
+let i2c = I2c::new(peripherals.I2C0, Default::default())?
+    .with_sda(peripherals.GPIO4)
+    .with_scl(peripherals.GPIO5)
+    .into_async();
+let mut aht10 = Aht10Async::new(AHT10_DEFAULT_ADDR, i2c, Delay); 
+aht10.calibrate().await?; // do calibrate is recommended
+let (humidity, temperature) = aht10.read().await?.decode(); // aht10 has no checksum support
+// example output: humidity = 67.25 %, temperature = 23.75 °C
+info!("humidity = {} %, temperature = {} °C", humidity, temperature);
+// read raw value without calc decode formula
+let humidity_raw = aht10.read().await?.humidity_raw; 
+```
+
+### AHT10 sync
+
+```rust
+use aht30::{AHT10_DEFAULT_ADDR, Aht10};
+use esp_hal::delay::Delay;
+use esp_hal::i2c::master::I2c;
+let i2c = I2c::new(peripherals.I2C0, Default::default())?
+    .with_sda(peripherals.GPIO4)
+    .with_scl(peripherals.GPIO5);
+let mut aht10 = Aht10::new(AHT10_DEFAULT_ADDR, i2c, Delay);
+aht10.calibrate()?;
+let (humidity, temperature) = aht10.read()?.decode();
+```
+
+### AHT20 / AHT25 / AHT30, async
+
+For sync usage, see aht10 example. Just remove `Async` suffix in name.
+
+```rust
+use aht30::{AHT20_DEFAULT_ADDR, Aht20Async};
+let mut aht20 = Aht20Async::new(AHT20_DEFAULT_ADDR, i2c, Delay);
+aht20.calibrate().await?; // do calibrate may not be required for models manufactured after 2022, but still recommended
+let (humidity, temperature) = aht20.read(true).await?.decode(); // enable checksum is recommended
+```
+
+### AHT40, async
+
+```rust
+use aht30::{AHT40_DEFAULT_ADDR, Aht40Async}; // it has different i2c address
+let mut aht40 = Aht40Async::new(AHT40_DEFAULT_ADDR, i2c, Delay);
+// aht40 has no calibrate function
+let (humidity, temperature) = aht40.read(true).await?.decode(); // enable checksum is recommended
+```
--- src/lib.rs
+++ src/lib.rs
@@ -24,13 +24,6 @@ fn crc8(data: &[u8]) -> u8 {
     crc
 }
 
-/// Decode humidity and temperature. For example, returns humidity = 67.5 (percent), temperature = 23.5 (celsius).
-fn decode(humidity_raw: u32, temperature_raw: u32) -> (f32, f32) {
-    let humidity = (humidity_raw as f32 / ((1 << 20) as f32)) * 100.0;
-    let temperature = (temperature_raw as f32 / ((1 << 20) as f32)) * 200.0 - 50.0;
-    (humidity, temperature)
-}
-
 /// Possible errors when interacting with the sensor.
 #[derive(Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
@@ -71,11 +64,73 @@ pub struct Aht10Async<I: I2cAsync, D: DelayNsAsync> {
     async(feature = "async")
 )]
 impl<I: I2cAsync, D: DelayNsAsync> Aht10Async<I, D> {
+    /// Requires >= 40ms after power-up, this function includes no delay, callers must ensure this timing.
     pub fn new(addr: u8, i2c: I, delay: D) -> Self {
         Self { addr, i2c, delay }
     }
-    // todo
+
+    /// Calibration is only necessary immediately after power-up. Not required during normal data acquisition.
+    pub async fn calibrate(&mut self) -> Result<(), SensorError> {
+        let mut status: [u8; 1] = [0; 1];
+        'done: {
+            for _ in 0..5 {
+                self.i2c.read(self.addr, &mut status).await?; // read only 8bit status
+                if status[0] & 0b1000 == 0b1000 {
+                    break 'done;
+                }
+                self.i2c.write(self.addr, &[0xE1, 0x08, 0x00]).await?;
+                self.delay.delay_ms(10).await;
+            }
+            return Err(SensorError::Timeout);
+        }
+        Ok(())
+    }
+
+    /// Reset sensor, without removing the power supply, takes <= 20ms to be done, this function includes no delay, callers must ensure this timing.
+    pub async fn soft_reset(&mut self) -> Result<(), SensorError> {
+        self.i2c.write(self.addr, &[0xBA]).await?;
+        Ok(())
+    }
+
+    /// Measure then read sensor, takes >= 80ms to be done. Unlike AHT20, there's no checksum.
+    pub async fn read(&mut self) -> Result<Aht10Measurement, SensorError> {
+        self.i2c.write(self.addr, &[0xAC, 0x33, 0x00]).await?;
+        self.delay.delay_ms(80).await;
+        let mut response: [u8; 6] = [0; 6]; // 48bits = status(8bit) + humidity(20bits) + temperature(20bits)
+        'done: {
+            for _ in 0..100 {
+                self.i2c.read(self.addr, &mut response[..1]).await?;
+                if response[0] & 0b1000_0000 == 0 {
+                    break 'done;
+                }
+                self.delay.delay_ms(5).await;
+            }
+            return Err(SensorError::Timeout);
+        }
+        self.i2c.read(self.addr, &mut response).await?;
+        let humidity_raw =
+            (response[1] as u32) << 12 | (response[2] as u32) << 4 | (response[3] as u32) >> 4;
+        let temperature_raw =
+            ((response[3] & 0x0f) as u32) << 16 | (response[4] as u32) << 8 | response[5] as u32;
+        Ok(Aht10Measurement {
+            humidity_raw,
+            temperature_raw,
+        })
+    }
 }
+pub struct Aht10Measurement {
+    pub humidity_raw: u32, // keep u32, decode is optional, some MCU without FPU will benefit from this
+    pub temperature_raw: u32,
+}
+impl Aht10Measurement {
+    /// Decode to `(humidity, temperature)`. For example, humidity = 67.25 (percent), temperature = 23.75 (celsius).
+    pub fn decode(&self) -> (f32, f32) {
+        let humidity = self.humidity_raw as f32 / ((1 << 20) as f32) * 100f32;
+        let temperature = self.temperature_raw as f32 / ((1 << 20) as f32) * 200f32 - 50f32;
+        (humidity, temperature)
+    }
+}
+pub const AHT10_DEFAULT_ADDR: u8 = 0x38;
 
 /// AHT20 / AHT25 / AHT30 sensor.
 #[maybe_async_cfg::maybe(
@@ -150,8 +205,8 @@ impl<I: I2cAsync, D: DelayNsAsync> Aht20Async<I, D> {
         }
         self.i2c.read(self.addr, &mut response).await?; // read whole response
         if checksum {
-            let received_crc = response[6]; // compare the calculated crc with the received crc
-            if received_crc != crc8(&response[..6]) {
+            // compare the calculated crc with the received crc
+            if response[6] != crc8(&response[..6]) {
                 return Err(SensorError::Checksum);
             }
         }
@@ -166,23 +221,78 @@ impl<I: I2cAsync, D: DelayNsAsync> Aht20Async<I, D> {
         })
     }
 }
-pub struct Aht20Measurement {
-    pub humidity_raw: u32, // keep u32, decode is optional, some MCU without FPU will benefit from this
-    pub temperature_raw: u32,
+pub use Aht10Measurement as Aht20Measurement; // aht20 and aht10 use same decode formula
+pub const AHT20_DEFAULT_ADDR: u8 = 0x38;
+
+/// AHT40 sensor.
+#[maybe_async_cfg::maybe(
+    idents(
+        Aht40Async(sync = "Aht40", async),
+        I2cAsync(sync = "I2c", async),
+        DelayNsAsync(sync = "DelayNs", async)
+    ),
+    sync(all()),
+    async(feature = "async")
+)]
+pub struct Aht40Async<I: I2cAsync, D: DelayNsAsync> {
+    addr: u8,
+    i2c: I,
+    delay: D,
 }
-impl Aht20Measurement {
-    /// Decode to `(humidity, temperature)`. For example, returns humidity = 67.5 (percent), temperature = 23.5 (celsius).
-    pub fn decode(&self) -> (f32, f32) {
-        decode(self.humidity_raw, self.temperature_raw)
+#[maybe_async_cfg::maybe(
+    idents(
+        Aht40Async(sync = "Aht40", async),
+        I2cAsync(sync = "I2c", async),
+        DelayNsAsync(sync = "DelayNs", async)
+    ),
+    sync(all()),
+    async(feature = "async")
+)]
+impl<I: I2cAsync, D: DelayNsAsync> Aht40Async<I, D> {
+    /// Requires >= 5ms (datasheet says this, but I recommend 100ms like AHT30) after power-up, this function includes no delay, callers must ensure this timing.
+    pub fn new(addr: u8, i2c: I, delay: D) -> Self {
+        Self { addr, i2c, delay }
+    }
+
+    /// Measure then read sensor, takes >= 80ms to be done. Enable checksum is recommended.
+    pub async fn read(&mut self, checksum: bool) -> Result<Aht40Measurement, SensorError> {
+        self.i2c.write(self.addr, &[0xFD]).await?; // measurement command, [this implement](https://github.com/libdriver/aht40/blob/84b7d3a74eff772cf9f61ff8bb189005f97ca6dd/src/driver_aht40.c#L319) and zh-cn datasheet says it should be single 0xFD, but [this datasheet](https://www.scribd.com/document/889704953/AHT40-datasheet-May-2024) says it should be [0xFD, 0xF6, 0xE0, 0x39, 0x32, 0x2F, 0x24, 0x1E, 0x15]
+        self.delay.delay_ms(80).await;
+        let mut response: [u8; 6] = [0; 6]; // 48bits = temperature(16bits) + crc(8bits) + humidity(16bits) + crc(8bits)
+        self.i2c.read(self.addr, &mut response).await?; // read whole response, aht40 has no status to read
+        if checksum {
+            // compare the calculated crc with the received crc
+            if response[2] != crc8(&response[0..2]) || response[5] != crc8(&response[3..5]) {
+                return Err(SensorError::Checksum);
+            }
+        }
+        let humidity_raw = (response[3] as u16) << 8 | (response[4] as u16);
+        let temperature_raw = (response[0] as u16) << 8 | (response[1] as u16);
+        Ok(Aht40Measurement {
+            humidity_raw,
+            temperature_raw,
+        })
     }
 }
-
-// TODO: AHT40 sensor.
+pub struct Aht40Measurement {
+    pub humidity_raw: u16, // keep u16, decode is optional, some MCU without FPU will benefit from this
+    pub temperature_raw: u16,
+}
+impl Aht40Measurement {
+    /// Decode to `(humidity, temperature)`. For example, humidity = 67.25 (percent), temperature = 23.75 (celsius).
+    pub fn decode(&self) -> (f32, f32) {
+        let humidity = self.humidity_raw as f32 / 65535f32 * 125f32 - 6f32;
+        let temperature = self.temperature_raw as f32 / 65535f32 * 175f32 - 45f32;
+        (humidity, temperature)
+    }
+}
+pub const AHT40_DEFAULT_ADDR: u8 = 0x44;
 
 /// - [AHT10.cpp](https://github.com/Thinary/AHT10/raw/refs/heads/master/src/Thinary_AHT10.cpp)
 /// - [AHT20_dvarrel.cpp](https://github.com/dvarrel/AHT20/raw/refs/heads/main/src/AHT20.cpp)
 /// - [AHT20_sparkfun.cpp](https://github.com/sparkfun/SparkFun_Qwiic_Humidity_AHT20_Arduino_Library/raw/refs/heads/main/src/SparkFun_Qwiic_Humidity_AHT20.cpp)
 /// - 官网提供的“说明书”是残缺的，缺少很多东西。“规格书(产品手册)”才是完整版。
+/// - [AHT10产品手册_A3_20201210.pdf](https://www.aosong.com/userfiles/files/media/AHT10%E4%BA%A7%E5%93%81%E6%89%8B%E5%86%8C%20A3%2020201210.pdf)
 /// - [AHT20_Data_Sheel_english_2021.pdf](https://www.aosong.com/userfiles/files/media/Data%20Sheet%20AHT20.pdf)
 /// - [AHT20产品规格书(中文版)A5.pdf](https://www.aosong.com/userfiles/files/media/AHT20%E4%BA%A7%E5%93%81%E8%A7%84%E6%A0%BC%E4%B9%A6(%E4%B8%AD%E6%96%87%E7%89%88)%20A5.pdf)
 /// - [DHT20产品规格书(中文版)A3-202409.pdf](https://www.aosong.com/uploadfiles/2025/02/20250228161634702.pdf)
@@ -199,7 +309,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let result = crc8(&[0x76, 0x54, 0x32, 0x10]);
+        let result = crc8(&[0x76, 0x54, 0x32, 0x10]); // we test on real hardware, not using mock
         assert_eq!(result, 0x21);
     }
 }
